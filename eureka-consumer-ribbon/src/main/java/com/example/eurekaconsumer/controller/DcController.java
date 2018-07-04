21c02aa29a630d78cbaf4a12d30900360e52914c
531183557@qq.com <531183557@qq.com>
2018-07-04 23:20:12 +0800

    euerka consumer and ribbon

---
+++ eureka-consumer-ribbon/src/main/java/com/example/eurekaconsumer/controller/DcController.java
@@ -0,0 +1,19 @@
+package com.example.eurekaconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DcController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping("/consumer")
+    public String dc(){
+        return restTemplate.getForObject("http://eureka-client/dc",String.class);
+    }
+
+}
